db118b37fcb0c18138d4f8d266db2a41e20d1952
Oleksandr Andriishyn <Alex.Andriishyn@icloud.com>
2015-04-12 14:33:03 -0400

    Final.. for now

--- src/source/BusinessAccount.java
+++ src/source/BusinessAccount.java
@@ -1,15 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//@misstiphany
+
 package source;
 
-/**
- *
- * @author Oleksandr
- */
-public class BusinessAccount extends Account
+public class BusinessAccount  extends Account
 {
+
 	
+	private double overdraftLimit = 10000;
+
+	public BusinessAccount (ACCT_TYPE, acctBalance, maxWithdrawal, monthlyFee, freeTransactionCount, interestRate)
+	{
+		this.setBusAcctNum(); //Assigns an acct Num.
+		this.busAcctBalance = 0.00;//initializes the account balance to 0
+	}
+
+	public BusinessAccount(double busAcctBalance)
+	{
+		this.setBusAcctNum();
+		this.busAcctBalance = busAcctBalance;
+	}
+
+	public double getOverdraftLimit()
+	{
+		return overdraftLimit = 10000;
+	}
+    
+
+            
+    
 }
+

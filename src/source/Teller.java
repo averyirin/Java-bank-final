55c9bb41cd6bcb206552f141045de3254414010e
averyirin <averyirin@gmail.com>
2015-04-12 19:25:54 -0400

    Teller V3.0

--- src/source/Teller.java
+++ src/source/Teller.java
@@ -1,6 +1,7 @@
 package source;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Teller {
@@ -23,25 +24,32 @@ public class Teller {
     }
 
     private void mainMenu() {
-        System.out.println("-Main-----------");
-        System.out.println("Input a command");
-        System.out.println("0. Exit program");
-        System.out.println("1. Create a new customer");
-        System.out.println("2. Search and manage customer information");
-        input = keyboard.next();
-        while (Integer.parseInt(input) != 0) {
-            switch (Integer.parseInt(input)) {
-                case 1:
-                    createCustomer();
-                    break;
-                case 2:
-                    lookupCustomer();
-                    break;
-                default:
-                    System.out.println("Please enter a valid command");
-                    break;
-            }
+        try {
+            System.out.println("-Main-----------");
+            System.out.println("Input a command");
+            System.out.println("0. Exit program");
+            System.out.println("1. Create a new customer");
+            System.out.println("2. Search and manage customer information");
             input = keyboard.next();
+            while (Integer.parseInt(input) != 0) {
+                switch (Integer.parseInt(input)) {
+                    case 1:
+                        createCustomer();
+                        break;
+                    case 2:
+                        lookupCustomer();
+                        break;
+                    default:
+                        System.out.println("Please enter a valid command");
+                        break;
+                }
+                input = keyboard.next();
+            }
+
+        } catch (InputMismatchException | IndexOutOfBoundsException | NumberFormatException e) {
+
+            System.out.println("Please enter a valid command");
+            mainMenu();
         }
 
         System.out.println("Program ended");
@@ -49,12 +57,14 @@ public class Teller {
     }
 
     private void createCustomer() {
-        System.out.println("-Customer Creation-----");
-        // set up customer information
 
+        System.out.println("-Customer Creation-----");
+
+        // set up customer information
         System.out.println("Customer Type");
         System.out.println("1. Regular 2. Student 3. Senior 4. Business");
         int custType = keyboard.nextInt();
+
         tempCustType = CustomerType.values()[custType - 1];
 
         System.out.println("First Name:");
@@ -71,6 +81,7 @@ public class Teller {
 
         customerList.add(tempCust);
         addAccount(tempCustType);
+
         mainMenu();
     }
 
@@ -190,32 +201,22 @@ public class Teller {
     private void displayCustomerList() {
 
         int i = 1;
-
-        String head = String.format("%s %20s %20s %20s %20s",
-                "ID", "First Name", "Last Name", "SIN", "Address");
-        System.out.println(head);
-
         for (Customer tempCust : customerList) {
-            System.out.println(i + " " + tempCust.toString());
+            System.out.println("ID " + i + " " + tempCust.toString());
             i++;
         }
 
     }
 
     private Customer getManageCustomer() {
-        try {
-            System.out.println("Please enter the customer's ID");
-            int id = keyboard.nextInt();
-            return customerList.get(id - 1);
-        } catch (IndexOutOfBoundsException e) {
-            return null;
-        }
+        System.out.println("Please enter the customer's ID");
+        int id = keyboard.nextInt();
+        return customerList.get(id - 1);
 
     }
 
     private void manageCustomer() {
-
-        System.out.println("-Edit " + tempCust.getFirstName() + " " + tempCust.getLastName() + "------");
+        System.out.println("-Manage " + tempCust.getFirstName() + " " + tempCust.getLastName() + "------");
         // cust.displayCustomerInfo();
         // cust.displayAccountInfo();
         System.out.println("Input a command");
@@ -253,7 +254,6 @@ public class Teller {
             }
             input = keyboard.next();
         }
-
         mainMenu();
     }
 
@@ -272,42 +272,37 @@ public class Teller {
     }
 
     private void chooseAccount(TransactionType transType) {
+
         // Display account info
         // cust.displayAccountInfo();
-        try {
-            getAllAccount();
-            System.out.println("Choose account for your " + transType);
-            int acctChoose = keyboard.nextInt();
-            tempAcct = tempCust.getAcctList().get(acctChoose - 1);
+        getAllAccount();
+        System.out.println("Choose account for your " + transType);
+        int acctChoose = keyboard.nextInt();
+        tempAcct = tempCust.getAcctList().get(acctChoose - 1);
 
-            System.out.println("Amount of your " + transType);
-            double amount = keyboard.nextDouble();
-            if (transType == TransactionType.DEPOSIT) {
-                tempAcct.deposit(amount);
-            } else if (transType == TransactionType.WITHDRAWAL) {
-                tempAcct.withdraw(amount);
+        System.out.println("Amount of your " + transType);
+        double amount = keyboard.nextDouble();
+        if (transType == TransactionType.DEPOSIT) {
+            tempAcct.deposit(amount);
+        } else if (transType == TransactionType.WITHDRAWAL) {
+            tempAcct.withdraw(amount);
 
-            }
-
-            System.out.println("Transaction successful");
-
-        } catch (IndexOutOfBoundsException e) {
-            System.out.println("No matching account found");
-
-        } finally {
-
-            manageCustomer();
         }
+
+        System.out.println("Transaction successful");
+
+        manageCustomer();
 
     }
 
     private void editInformation() {
+        System.out.println("-Edit Information of " + tempCust.getFirstName() + " " + tempCust.getLastName() + "------");
+        System.out.println(tempCust);
 
-        System.out.println("-Edit Information------");
         // display account info
         // cust.displayCustomerInfo();
         System.out.println("Input a command");
-        System.out.println("0. Back to main");
+        System.out.println("0. Back to manage customer");
         System.out.println("1. Edit First Name");
         System.out.println("2. Edit Last Name");
         System.out.println("3. Edit Address");
@@ -317,30 +312,41 @@ public class Teller {
         while (Integer.parseInt(input) != 0) {
             switch (Integer.parseInt(input)) {
                 case 1:
-                    //edit first names
-                    String fName = keyboard.next();
+                    //edit first names  
+                    System.out.println("New first name:");
+                    String firstName = keyboard.next();
+                    tempCust.setFirstName(firstName);
                     break;
                 case 2:
                     //edit last name
+                    System.out.println("New last name:");
+                    String lastName = keyboard.next();
+                    tempCust.setLastName(lastName);
                     break;
                 case 3:
                     //edit address
+                    System.out.println("New address:");
+                    keyboard.nextLine();
+                    String address = keyboard.nextLine();
+                    tempCust.setAddress(address);
                     break;
                 case 4:
-                    // create account
-                    break;
-                case 5:
-                    editInformation();
+                    // edit SIN
+                    System.out.println("New SIN:");
+                    String sin = keyboard.next();
+                    tempCust.setSin(sin);
                     break;
                 default:
                     System.out.println("Please enter a valid command");
-                    manageCustomer();
+
                     break;
             }
+
             input = keyboard.next();
         }
 
         manageCustomer();
+
     }
 
 }
